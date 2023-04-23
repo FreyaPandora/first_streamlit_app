2ef4d42aeef27c6091e2f7b8e7a4992727ab17d4
FreyaPandora <107923822+FreyaPandora@users.noreply.github.com>
2023-04-23 20:46:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
-import pandas
-import requests
+#import pandas
+#import requests
 import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.header('Breakfast Menu')
@@ -35,6 +36,8 @@ df = streamlit.dataframe(fruityvice_normalized)
 #add another fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
