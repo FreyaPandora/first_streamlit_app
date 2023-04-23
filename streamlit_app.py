f0e4448c79eb7db1461c5c56251d1bea0fd1e55e
FreyaPandora <107923822+FreyaPandora@users.noreply.github.com>
2023-04-23 14:40:24 +0100

    Update streamlit_app.py
    trying to append

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,21 +28,19 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
 #takes the content of the api and normalises it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # turns it into a dataframe to display as a table
 df = streamlit.dataframe(fruityvice_normalized)
-
 #add another fruit
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thanks for adding ', add_my_fruit)
 fruityvice_response2 = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #takes the content of the api and normalises it
 fruityvice_normalized2 = pandas.json_normalize(fruityvice_response2.json())
 # turns it into a dataframe to display as a table
-df2 = streamlit.dataframe(fruityvice_normalized2)
+streamlit.df.append(fruityvice_normalized2)
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
